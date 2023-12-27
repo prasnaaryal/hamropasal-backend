be12add9f548daedff60f1da50ebec765d65e46c
prasnaaryal <prasna101@gmail.com>
2023-12-28 00:07:13 +0545

    created add product api

--- index.js
+++ index.js
@@ -84,4 +84,30 @@ app.post("/login", (req, res) => {
   });
 });
 
+//product section
+const schemaProduct=mongoose.Schema({
+  name: String,
+  category: String,
+  image: String,
+  price: String,
+  description: String,
+})
+const productModel=mongoose.model("product",schemaProduct)
+
+//save product in data api
+app.post("/uploadProduct",async(req,res)=>{
+  console.log(req.body)
+  const data=await productModel(req.body)
+  const datasave=await data.save()
+
+
+  res.send({message:"Uploaded Successfully"})
+})
+
+
+//get products api
+app.get("/product",async(req,res)=>{
+  const data=await productModel.find({})
+  res.send(JSON.stringify(data))
+})
 app.listen(PORT, () => console.log("server is running at: " + PORT));
